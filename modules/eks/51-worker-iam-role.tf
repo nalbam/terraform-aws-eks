d5f32d6b8510893312c6a22682cea305c34399f1
nalbam <me@nalbam.com>
2018-11-13 20:00:22 +0900

    AmazonEC2FullAccess

--- modules/eks/51-worker-iam-role.tf
+++ modules/eks/51-worker-iam-role.tf
@@ -19,14 +19,14 @@ resource "aws_iam_role" "worker" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "worker-AmazonEKSWorkerNodePolicy" {
-  role       = "${aws_iam_role.worker.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
 resource "aws_iam_role_policy_attachment" "worker-AmazonEKS_CNI_Policy" {
   role       = "${aws_iam_role.worker.name}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "worker-AmazonEKSWorkerNodePolicy" {
+  role       = "${aws_iam_role.worker.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "worker-AmazonEC2ContainerRegistryReadOnly" {
@@ -34,9 +34,9 @@ resource "aws_iam_role_policy_attachment" "worker-AmazonEC2ContainerRegistryRead
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "worker-AutoScalingFullAccess" {
+resource "aws_iam_role_policy_attachment" "worker-AmazonEC2FullAccess" {
   role       = "${aws_iam_role.worker.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
 resource "aws_iam_instance_profile" "worker" {
