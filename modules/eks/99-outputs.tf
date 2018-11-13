98419cb046b1b149359e09395a0f95f097a57259
nalbam <me@nalbam.com>
2018-11-13 21:46:21 +0900

    r5

--- modules/eks/99-outputs.tf
+++ modules/eks/99-outputs.tf
@@ -1,7 +1,7 @@
 # output
 
 data "template_file" "kube_config" {
-  template = "${file("${path.module}/data/kube_config.yml")}"
+  template = "${file("${path.module}/data/kube_config.yaml")}"
 
   vars {
     CERTIFICATE     = "${aws_eks_cluster.cluster.certificate_authority.0.data}"
@@ -11,7 +11,7 @@ data "template_file" "kube_config" {
 }
 
 data "template_file" "aws_auth" {
-  template = "${file("${path.module}/data/aws_auth.yml")}"
+  template = "${file("${path.module}/data/aws_auth.yaml")}"
 
   vars {
     AWS_IAM_ROLE_ARN = "${aws_iam_role.worker.arn}"
@@ -19,22 +19,22 @@ data "template_file" "aws_auth" {
 }
 
 data "template_file" "aws_ebs_gp2" {
-  template = "${file("${path.module}/data/aws_ebs_gp2.yml")}"
+  template = "${file("${path.module}/data/aws_ebs_gp2.yaml")}"
 }
 
 resource "local_file" "kube_config" {
   content  = "${data.template_file.kube_config.rendered}"
-  filename = "${path.cwd}/.output/kube_config.yml"
+  filename = "${path.cwd}/.output/kube_config.yaml"
 }
 
 resource "local_file" "aws_auth" {
   content  = "${data.template_file.aws_auth.rendered}"
-  filename = "${path.cwd}/.output/aws_auth.yml"
+  filename = "${path.cwd}/.output/aws_auth.yaml"
 }
 
 resource "local_file" "aws_ebs_gp2" {
   content  = "${data.template_file.aws_ebs_gp2.rendered}"
-  filename = "${path.cwd}/.output/aws_ebs_gp2.yml"
+  filename = "${path.cwd}/.output/aws_ebs_gp2.yaml"
 }
 
 locals {
@@ -44,13 +44,13 @@ locals {
 aws configure set default.region ${var.region}
 
 # kube config
-mkdir -p ~/.kube && cat .output/kube_config.yml > ~/.kube/config
+mkdir -p ~/.kube && cat .output/kube_config.yaml > ~/.kube/config
 
 # aws auth
-kubectl apply -f .output/aws_auth.yml
+kubectl apply -f .output/aws_auth.yaml
 
 # aws ebs gp2
-kubectl apply -f .output/aws_ebs_gp2.yml
+kubectl apply -f .output/aws_ebs_gp2.yaml
 
 # get
 kubectl get node -o wide
