b5ba5bbca9d607be2c6b567e3b4ed21be20982e7
nalbam <me@nalbam.com>
2018-11-17 10:45:42 +0900

    aws_launch_template

--- modules/eks/55-eks-worker.tf
+++ modules/eks/55-eks-worker.tf
@@ -22,40 +22,65 @@ set -o xtrace
 EOF
 }
 
-resource "aws_launch_configuration" "worker" {
-  name_prefix                 = "${local.lower_name}"
-  iam_instance_profile        = "${aws_iam_instance_profile.worker.name}"
-  image_id                    = "${data.aws_ami.worker.id}"
-  instance_type               = "${var.instance_type}"
-  security_groups             = ["${aws_security_group.worker.id}"]
-  user_data_base64            = "${base64encode(local.userdata)}"
-  associate_public_ip_address = true
+# resource "aws_launch_configuration" "worker" {
+#   name_prefix                 = "${local.lower_name}"
+#   image_id                    = "${data.aws_ami.worker.id}"
+#   instance_type               = "${var.instance_type}"
+#   iam_instance_profile        = "${aws_iam_instance_profile.worker.name}"
+#   user_data_base64            = "${base64encode(local.userdata)}"
+#   associate_public_ip_address = true
 
-  lifecycle {
-    create_before_destroy = true
+#   security_groups = ["${aws_security_group.worker.id}"]
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+resource "aws_launch_template" "worker" {
+  name_prefix            = "${local.lower_name}"
+  image_id               = "${data.aws_ami.worker.id}"
+  instance_type          = "${var.instance_type}"
+  user_data              = "${base64encode(local.userdata)}"
+  # vpc_security_group_ids = ["${aws_security_group.worker.id}"]
+
+  iam_instance_profile {
+    name = "${aws_iam_instance_profile.worker.name}"
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = ["${aws_security_group.worker.id}"]
+  }
+
+  instance_market_options {
+    market_type = "spot"
   }
 }
 
 resource "aws_autoscaling_group" "worker" {
-  name                 = "${local.lower_name}"
-  desired_capacity     = "${var.desired}"
-  min_size             = "${var.min}"
-  max_size             = "${var.max}"
-  vpc_zone_identifier  = ["${aws_subnet.public.*.id}"]
-  launch_configuration = "${aws_launch_configuration.worker.id}"
+  name                = "${local.lower_name}"
+  desired_capacity    = "${var.desired}"
+  min_size            = "${var.min}"
+  max_size            = "${var.max}"
+  vpc_zone_identifier = ["${aws_subnet.public.*.id}"]
 
+  # launch_configuration = "${aws_launch_configuration.worker.id}"
+
+  launch_template = {
+    id      = "${aws_launch_template.worker.id}"
+    version = "${aws_launch_template.worker.latest_version}"
+  }
   tag {
     key                 = "Name"
     value               = "${local.lower_name}"
     propagate_at_launch = true
   }
-
   tag {
     key                 = "kubernetes.io/cluster/${local.lower_name}"
     value               = "owned"
     propagate_at_launch = true
   }
-
   tag {
     key                 = "k8s.io/cluster-autoscaler/enabled"
     value               = ""
