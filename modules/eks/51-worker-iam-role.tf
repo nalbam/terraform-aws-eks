d547939fdfd659ee7cc4c8a342608fce68a2b3e6
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-01-14 17:41:54 +0900

    AutoScalingFullAccess

--- modules/eks/51-worker-iam-role.tf
+++ modules/eks/51-worker-iam-role.tf
@@ -39,58 +39,7 @@ resource "aws_iam_role_policy_attachment" "worker-AmazonEC2ContainerRegistryRead
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# resource "aws_iam_role_policy_attachment" "worker-AutoScalingFullAccess" {
-#   role       = "${aws_iam_role.worker.name}"
-#   policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
-# }
-
-resource "aws_iam_role_policy_attachment" "worker-AutoScaling" {
-  policy_arn = "${aws_iam_policy.worker_autoscaling.arn}"
+resource "aws_iam_role_policy_attachment" "worker-AutoScalingFullAccess" {
   role       = "${aws_iam_role.worker.name}"
-}
-
-resource "aws_iam_policy" "worker_autoscaling" {
-  name_prefix = "${local.lower_name}-worker-autoscaling"
-  policy      = "${data.aws_iam_policy_document.worker_autoscaling.json}"
-}
-
-data "aws_iam_policy_document" "worker_autoscaling" {
-  statement {
-    sid    = "eksWorkerAutoscalingAll"
-    effect = "Allow"
-
-    actions = [
-      "autoscaling:DescribeAutoScalingGroups",
-      "autoscaling:DescribeAutoScalingInstances",
-      "autoscaling:DescribeLaunchConfigurations",
-      "autoscaling:DescribeTags",
-    ]
-
-    resources = ["*"]
-  }
-
-  statement {
-    sid    = "eksWorkerAutoscalingOwn"
-    effect = "Allow"
-
-    actions = [
-      "autoscaling:SetDesiredCapacity",
-      "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "autoscaling:UpdateAutoScalingGroup",
-    ]
-
-    resources = ["*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${local.lower_name}"
-      values   = ["owned"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
-      values   = ["true"]
-    }
-  }
+  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
 }
