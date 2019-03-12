ccc0df19eb1aee8d0ad0ec1b952f2395ac6e6d71
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-03-12 10:46:29 +0900

    fix key_name

--- modules/eks/54-worker-key-pair.tf
+++ modules/eks/54-worker-key-pair.tf
@@ -2,6 +2,6 @@
 
 resource "aws_key_pair" "worker" {
   count      = "${var.key_path != "" ? 1 : 0}"
-  key_name   = "${local.lower_name}-worker"
+  key_name   = "${local.upper_name}-WORKER"
   public_key = "${file(var.key_path)}"
 }
