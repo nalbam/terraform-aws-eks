14df63230ea4c18fe199eef5e6eafa90a8e12ac6
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-04-16 11:53:22 +0900

    add cluster version

--- modules/eks/35-cluster.tf
+++ modules/eks/35-cluster.tf
@@ -3,6 +3,7 @@
 resource "aws_eks_cluster" "cluster" {
   name     = "${local.lower_name}"
   role_arn = "${aws_iam_role.cluster.arn}"
+  version  = "${var.kubernetes_version}"
 
   vpc_config {
     subnet_ids         = ["${var.subnet_ids}"]
