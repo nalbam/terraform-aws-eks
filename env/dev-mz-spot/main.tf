44e778211b62d4c29f169faac59281ac7bf0057b
nalbam <me@nalbam.com>
2019-08-23 17:44:32 +0900

    add spot

---
+++ env/dev-mz-spot/main.tf
@@ -0,0 +1,88 @@
+# eks
+
+terraform {
+  backend "s3" {
+    region = "ap-northeast-2"
+    bucket = "terraform-mz-seoul"
+    key    = "eks-spot.tfstate"
+  }
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "eks" {
+  source = "../../modules/eks"
+
+  region = var.region
+  city   = var.city
+  stage  = var.stage
+  name   = var.name
+  suffix = var.suffix
+
+  vpc_id = var.vpc_id
+
+  subnet_ids = var.subnet_ids
+
+  kubernetes_version = "1.13"
+
+  allow_ip_address = [
+    "10.10.1.0/24", # bastion
+  ]
+
+  # aws-auth
+  map_roles = [
+    {
+      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/seoul-dev-demo-bastion"
+      username = "iam-role-bastion"
+      group    = "system:masters"
+    },
+  ]
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/jungyoul.yu"
+      username = "jungyoul.yu"
+      group    = "system:masters"
+    },
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/developer"
+      username = "developer"
+      group    = ""
+    },
+  ]
+
+  # worker
+  launch_configuration_enable = false
+  launch_template_enable      = true
+  launch_each_subnet          = false
+
+  associate_public_ip_address = false
+
+  instance_type = "m5.xlarge"
+
+  mixed_instances = ["c5.xlarge", "r5.xlarge"]
+
+  volume_size = "32"
+
+  min = "1"
+  max = "5"
+
+  on_demand_base = "0"
+  on_demand_rate = "0"
+
+  key_name = "nalbam-seoul"
+
+  # efs
+  launch_efs_enable = true
+
+  # # s3
+  # buckets = [
+  #   "argo",
+  # ]
+}
+
+output "config" {
+  value = module.eks.config
+}
---
+++ env/dev-mz-spot/variables.tf
@@ -0,0 +1,38 @@
+variable "region" {
+  default = "ap-northeast-2"
+}
+
+variable "city" {
+  default = "seoul"
+}
+
+variable "stage" {
+  default = "dev"
+}
+
+variable "name" {
+  default = "spot"
+}
+
+variable "suffix" {
+  default = "eks"
+}
+
+variable "vpc_id" {
+  default = "vpc-018a61f4f7cfedf46"
+}
+
+variable "subnet_ids" {
+  default = [
+    "subnet-053ee10649577323f",
+    "subnet-00bbcda81a83ed5b3",
+    "subnet-0490973094e02cb6c",
+  ]
+}
+
+data "aws_caller_identity" "current" {
+}
+
+locals {
+  full_name = "${var.city}-${var.stage}-${var.name}-${var.suffix}"
+}
