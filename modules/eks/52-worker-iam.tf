434bfa8055d4001e8d57c4c135289ad98f7cb0f2
nalbam <me@nalbam.com>
2019-09-02 19:11:18 +0900

    kube2iam

--- modules/eks/52-worker-iam.tf
+++ modules/eks/52-worker-iam.tf
@@ -19,3 +19,17 @@ resource "aws_iam_role_policy_attachment" "worker-AmazonEC2ContainerRegistryRead
 #   role       = "${module.worker.iam_role_name}"
 #   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 # }
+
+resource "aws_iam_role_policy" "worker_kube2iam" {
+  name   = "EKSNodeKube2IAMPolicy"
+  role   = module.worker.iam_role_name
+  policy = "${data.aws_iam_policy_document.worker_kube2iam.json}"
+}
+
+data "aws_iam_policy_document" "worker_kube2iam" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = ["*"]
+  }
+}
