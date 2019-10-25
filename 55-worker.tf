08c4eee1d27cee462fa7447c808eb604682071d3
nalbam <me@nalbam.com>
2019-10-25 16:40:46 +0900

    terraform-aws-asg?ref=v0.12.11

--- 55-worker.tf
+++ 55-worker.tf
@@ -1,7 +1,7 @@
 # worker
 
 module "worker" {
-  source = "github.com/nalbam/terraform-aws-asg?ref=v0.12.8"
+  source = "github.com/nalbam/terraform-aws-asg?ref=v0.12.11"
 
   name = "${var.name}-worker"
 
