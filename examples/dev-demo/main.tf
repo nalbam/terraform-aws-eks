fbaa1cb5e80c86bf0f563c1e2a9d84c865095d19
nalbam <me@nalbam.com>
2019-10-28 10:42:13 +0900

    add dynamodb_table

--- examples/dev-demo/main.tf
+++ examples/dev-demo/main.tf
@@ -5,6 +5,8 @@ terraform {
     region = "ap-northeast-2"
     bucket = "terraform-nalbam-seoul"
     key    = "eks-demo.tfstate"
+    # encrypt        = true
+    # dynamodb_table = "terraform-resource-lock"
   }
   required_version = ">= 0.12"
 }
