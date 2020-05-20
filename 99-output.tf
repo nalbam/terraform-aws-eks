dbe3d7d42a009fb1c832086aefdb68490a2de6e3
nalbam <me@nalbam.com>
2020-05-20 18:10:21 +0900

    oidc_issuer

--- 99-output.tf
+++ 99-output.tf
@@ -16,12 +16,20 @@ output "endpoint" {
   value = data.aws_eks_cluster.cluster.endpoint
 }
 
-output "certificate_authority" {
-  value = data.aws_eks_cluster.cluster.certificate_authority.0.data
+# output "certificate_authority" {
+#   value = data.aws_eks_cluster.cluster.certificate_authority.0.data
+# }
+
+# output "token" {
+#   value = data.aws_eks_cluster_auth.cluster.token
+# }
+
+output "oidc_issuer" {
+  value = join("", aws_eks_cluster.cluster.*.identity.0.oidc.0.issuer)
 }
 
-output "token" {
-  value = data.aws_eks_cluster_auth.cluster.token
+output "oidc_issuer_arn" {
+  value = join("", aws_iam_openid_connect_provider.oidc_provider.*.arn)
 }
 
 output "security_group_id" {
