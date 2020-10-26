453b87cb14bcc61603a58db5e5def37401bffbf6
nalbam <me@nalbam.com>
2020-10-26 15:06:16 +0900

    cluster_security_groups

--- 99-output.tf
+++ 99-output.tf
@@ -45,8 +45,8 @@ output "cluster_role_name" {
   value = aws_iam_role.cluster.name
 }
 
-output "cluster_security_group_id" {
-  value = aws_security_group.cluster.id
+output "cluster_security_groups" {
+  value = [aws_security_group.cluster.id]
 }
 
 output "worker_ami_id" {
