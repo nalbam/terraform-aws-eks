f5afca14646488b1466589c89c8daa62d1b65360
nalbam <me@nalbam.com>
2020-10-27 14:02:03 +0900

    default cluster_log_types

--- 00-variable.tf
+++ 00-variable.tf
@@ -34,7 +34,7 @@ variable "endpoint_public_access" {
 variable "cluster_log_types" {
   description = "A list of the desired control plane logging to enable. For more information, see Amazon EKS Control Plane Logging documentation (https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html)"
   type        = list(string)
-  default     = []
+  default     = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   # api, audit, authenticator, controllerManager, scheduler
 }
 
@@ -47,7 +47,7 @@ variable "cluster_log_kms_key_id" {
 variable "cluster_log_retention_in_days" {
   description = "Number of days to retain log events. Default retention - 90 days."
   type        = number
-  default     = 7
+  default     = 14
 }
 
 variable "allow_ip_address" {
