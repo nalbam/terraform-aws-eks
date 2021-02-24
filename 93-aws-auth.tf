a30ff0d51ec748d6999aa6a1ad8dc539099d86e0
nalbam <me@nalbam.com>
2021-02-24 23:31:52 +0900

    load_config_file

--- 93-aws-auth.tf
+++ 93-aws-auth.tf
@@ -4,7 +4,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  # load_config_file       = false
 }
 
 resource "kubernetes_config_map" "aws_auth" {
@@ -12,16 +12,6 @@ resource "kubernetes_config_map" "aws_auth" {
     name      = "aws-auth"
     namespace = "kube-system"
   }
-
-#   data = {
-#     mapRoles = <<EOF
-# ${join("", distinct(concat(data.template_file.aws_auth_workers.*.rendered)))}
-# ${join("", distinct(concat(data.template_file.aws_auth_roles.*.rendered)))}
-# EOF
-#     mapUsers = <<EOF
-# ${join("", distinct(concat(data.template_file.aws_auth_users.*.rendered)))}
-# EOF
-#   }
 
   data = {
     mapRoles = yamlencode(local.roles)
