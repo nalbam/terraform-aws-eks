38c1793861492160c26d4d75601267d035ec65a7
nalbam <me@nalbam.com>
2022-02-11 11:28:04 +0900

    fix sqs policy service order

--- 38-worker-sqs.tf
+++ 38-worker-sqs.tf
@@ -1,30 +1,28 @@
 # aws-node-termination-handler
 
-data "aws_iam_policy_document" "sqs" {
-  statement {
-    effect = "Allow"
-    principals {
-      type = "Service"
-      identifiers = [
-        "events.amazonaws.com",
-        "sqs.amazonaws.com",
-      ]
-    }
-    actions = [
-      "sqs:SendMessage",
-    ]
-    resources = [
-      "arn:aws:sqs:${local.region}:${local.account_id}:${local.worker_name}",
-    ]
-  }
-}
-
 resource "aws_sqs_queue" "worker" {
   name = local.worker_name
 
   message_retention_seconds = 300
 
-  policy = data.aws_iam_policy_document.sqs.json
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "events.amazonaws.com",
+          "sqs.amazonaws.com"
+        ]
+      },
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:${local.region}:${local.account_id}:${local.worker_name}"
+    }
+  ]
+}
+POLICY
 
   tags = {
     "Name"                                      = local.worker_name
