b420e613c1d310ea6b17e36ab3197cf371912f4d
nalbam <me@nalbam.com>
2022-02-11 13:21:42 +0900

    sqs aws_iam_policy_document

--- 38-worker-sqs.tf
+++ 38-worker-sqs.tf
@@ -1,10 +1,30 @@
 # aws-node-termination-handler
 
+# data "aws_iam_policy_document" "sqs" {
+#   statement {
+#     effect = "Allow"
+#     principals {
+#       type = "Service"
+#       identifiers = [
+#         "events.amazonaws.com",
+#         "sqs.amazonaws.com",
+#       ]
+#     }
+#     actions = [
+#       "sqs:SendMessage",
+#     ]
+#     resources = [
+#       "arn:aws:sqs:${local.region}:${local.account_id}:${local.worker_name}",
+#     ]
+#   }
+# }
+
 resource "aws_sqs_queue" "worker" {
   name = local.worker_name
 
   message_retention_seconds = 300
 
+  # policy = data.aws_iam_policy_document.sqs.json
   policy = <<POLICY
 {
   "Version": "2012-10-17",
