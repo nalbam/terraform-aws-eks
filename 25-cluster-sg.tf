550f20f5fde5bfd5b3400fb735b7533ba4cb8e8e
nalbam <me@nalbam.com>
2022-04-04 21:17:31 +0900

    add cni_ipv6_policy

--- 25-cluster-sg.tf
+++ 25-cluster-sg.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "cluster" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = var.ip_family == "ipv6" ? ["::/0"] : null
   }
 
   tags = {
--- 35-worker-sg.tf
+++ 35-worker-sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "worker" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = var.ip_family == "ipv6" ? ["::/0"] : null
   }
 
   tags = {
