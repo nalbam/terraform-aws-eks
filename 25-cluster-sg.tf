f6588df18b7a01384025864eb19406823d7ca777
nalbam <me@nalbam.com>
2024-02-01 14:48:14 +0900

    fix typo sgs

--- 25-cluster-sg.tf
+++ 25-cluster-sg.tf
@@ -25,47 +25,47 @@ resource "aws_security_group" "cluster" {
 }
 
 resource "aws_security_group_rule" "cluster_worker" {
+  type                     = "ingress"
   description              = format("%s - worker 443", local.cluster_name)
   security_group_id        = aws_security_group.cluster.id
   from_port                = 443
   to_port                  = 443
   protocol                 = "TCP"
   source_security_group_id = aws_security_group.worker.id
-  type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "cluster_sslvpn" {
   count = var.sslvpn_name != "" ? 1 : 0
 
+  type              = "ingress"
   description       = format("%s - sslvpn 443", local.cluster_name)
   security_group_id = aws_security_group.cluster.id
   from_port         = 443
   to_port           = 443
   protocol          = "TCP"
   prefix_list_ids   = data.aws_ec2_managed_prefix_list.sslvpn.*.id
-  type              = "ingress"
 }
 
 resource "aws_security_group_rule" "cluster_prefix_list" {
   count = length(var.allow_prefix_list_ids)
 
+  type              = "ingress"
   description       = format("%s - prefix_list 443", local.cluster_name)
   security_group_id = aws_security_group.cluster.id
   from_port         = 443
   to_port           = 443
   protocol          = "TCP"
   prefix_list_ids   = [var.allow_prefix_list_ids[count.index]]
-  type              = "ingress"
 }
 
 resource "aws_security_group_rule" "cluster_cidr" {
   count = length(var.allow_cidr_cluster)
 
+  type              = "ingress"
   description       = format("%s - cidr 443", local.cluster_name)
   security_group_id = aws_security_group.cluster.id
   from_port         = 443
   to_port           = 443
   protocol          = "TCP"
   cidr_blocks       = [var.allow_cidr_cluster[count.index]]
-  type              = "ingress"
 }
--- 35-worker-sg.tf
+++ 35-worker-sg.tf
@@ -23,79 +23,79 @@ resource "aws_security_group" "worker" {
 }
 
 resource "aws_security_group_rule" "worker_cluster" {
+  type                     = "ingress"
   description              = format("%s - cluster 1025-65535", local.worker_name)
   security_group_id        = aws_security_group.worker.id
   from_port                = 1025
   to_port                  = 65535
   protocol                 = "TCP"
   source_security_group_id = aws_security_group.cluster.id
-  type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "worker_cluster_443" {
+  type                     = "ingress"
   description              = format("%s - cluster 443", local.worker_name)
   security_group_id        = aws_security_group.worker.id
   from_port                = 443
   to_port                  = 443
   protocol                 = "TCP"
   source_security_group_id = aws_security_group.cluster.id
-  type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "worker_worker" {
+  type                     = "ingress"
   description              = format("%s - worker all", local.worker_name)
   security_group_id        = aws_security_group.worker.id
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
   source_security_group_id = aws_security_group.worker.id
-  type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "worker_source" {
   count = length(var.worker_source_sgs)
 
+  type                     = "ingress"
   description              = format("%s - worker source", local.worker_name)
+  security_group_id        = aws_security_group.worker.id
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
-  security_group_id        = aws_security_group.worker.id
   source_security_group_id = var.worker_source_sgs[count.index]
-  type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "worker_prefix_list" {
   count = length(var.allow_prefix_list_ids)
 
+  type              = "ingress"
   description       = format("%s - prefix_list", local.worker_name)
   security_group_id = aws_security_group.worker.id
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   prefix_list_ids   = [var.allow_prefix_list_ids[count.index]]
-  type              = "ingress"
 }
 
 resource "aws_security_group_rule" "worker_ports_internal" {
   count = length(var.worker_ports_internal)
 
+  type              = "ingress"
   description       = format("%s - internal ports (%s)", local.worker_name, var.worker_ports_internal[count.index])
   security_group_id = aws_security_group.worker.id
   from_port         = var.worker_ports_internal[count.index]
   to_port           = var.worker_ports_internal[count.index]
   protocol          = "TCP"
   cidr_blocks       = var.allow_cidr_internal
-  type              = "ingress"
 }
 
 resource "aws_security_group_rule" "worker_ports_public" {
   count = length(var.worker_ports_public)
 
+  type              = "ingress"
   description       = format("%s - public ports (%s)", local.worker_name, var.worker_ports_public[count.index])
   security_group_id = aws_security_group.worker.id
   from_port         = var.worker_ports_public[count.index]
   to_port           = var.worker_ports_public[count.index]
   protocol          = "TCP"
   cidr_blocks       = var.allow_cidr_public
-  type              = "ingress"
 }
